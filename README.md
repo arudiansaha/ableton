15019a9ba7f75de6debb4d284a784ceefb69a7ed
Rizky Ardiansyah <rizkyardiansyah@disroot.org>
2023-08-27 16:56:05 +0700

    Initial cofiguration after vite init.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ableton - About Page (Frontend Practice)
---
+++ src/views/templates/App.tsx
@@ -0,0 +1,11 @@
+import About from '@/views/pages/About';
+
+function App() {
+  return (
+    <main>
+      <About />
+    </main>
+  );
+}
+
+export default App;
