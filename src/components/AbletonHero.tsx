0166591dd4fdade628d3c6647c44aa05a2cfb372
Rizky Ardiansyah <rizkyardiansyah@disroot.org>
2023-09-04 15:52:23 +0700

    Add implicit config and content in about pages.
    - Set `noImplicitAny` to `true` to avoid `any` types
    - Add ableton hero in about
    - Add section description in about

---
+++ src/components/AbletonHero.tsx
@@ -0,0 +1,10 @@
+function AbletonHero() {
+  return (
+    <section className="about__hero">
+      <h1>Ableton</h1>
+      <img src="https://ableton-production.imgix.net/about/header.jpg?fit=crop&auto=format&fm=jpg" alt="" />
+    </section>
+  );
+}
+
+export default AbletonHero;
