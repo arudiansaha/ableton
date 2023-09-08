3b1445deed4c38a73a0bbd73663d801788ae6ef1
Rizky Ardiansyah <rizkyardiansyah@disroot.org>
2023-09-09 04:55:06 +0700

    Refactor padding and replace "'" with unicode.

--- src/views/pages/About.tsx
+++ src/views/pages/About.tsx
@@ -35,7 +35,7 @@ function About() {
             We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.
           </h2>
           <p>
-            Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We’ve learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don&apos;t always agree with each other, but opinion and debate are valued and openly encouraged.
+            Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We&apos;ve learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don&apos;t always agree with each other, but opinion and debate are valued and openly encouraged.
           </p>
         </section>
         <section className="about__section">
